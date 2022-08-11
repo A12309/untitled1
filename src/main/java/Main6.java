98bdf93559ae68e78d23d4339da97544dd7babac
South <south123@mail.ru>
2022-08-12 02:01:11 +0300

    6 task not done. But algoritm is done

--- src/main/java/Main6.java
+++ src/main/java/Main6.java
@@ -1,16 +1,72 @@
-import java.util.Scanner;
+import javafx.util.Pair;
+
+import java.util.*;
 
 public class Main6 {
-    public static void main(String[] args) {
-        class Lift {
-            private int f;
-            private int s;
+    private static Set<Lift> dontMoveLifts = new HashSet<>();
+    private static ArrayList<Integer> tracks = new ArrayList<>();
+
+    public static class Lift {
+        private int s;
+        private int f;
+        private int i;
+        private ArrayList<Lift> liftsInFloor = new ArrayList<>();
+       // private Pair<Integer, Lift> liftsInFloor = new Pair<>();
+
+
+        public void setI(int i) {
+            this.i = i;
         }
+
+        public Lift(int s, int f) {
+            this.s = s;
+            this.f = f;
+        }
+
+        public void findLifts(int i, ArrayList<Lift> lifts) {
+            i++;
+            boolean isHighestFloor = true;
+            for (Lift lift : lifts) {
+                if (!lift.equals(this) && !liftsInFloor.contains(lift) && !dontMoveLifts.contains(lift)) {
+                    if (lift.getS() == f) {
+                        isHighestFloor = false;
+                        lift.setI(i);
+                        liftsInFloor.add(lift);
+                     //   liftsInFloor.put(i, lift);
+                        if (lift.getF() == lift.getS()) {
+                            dontMoveLifts.add(lift);
+                        }
+                        lift.findLifts(i, lifts);
+
+                    }
+                }
+
+            }
+            if (isHighestFloor)
+                tracks.add(i - 1);
+        }
+
+        public int getS() {
+            return s;
+        }
+
+        public int getF() {
+            return f;
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Lift> lifts = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
 
         for (int i = 0; i < n; i++) {
-
+            lifts.add(new Lift(scanner.nextInt(), scanner.nextInt()));
+            scanner.nextLine();
         }
+        Lift rootLift = new Lift(-1, 0);
+        rootLift.findLifts(0, lifts);
+        Collections.sort(tracks);
+        System.out.println(tracks.get(tracks.size() - 1));
     }
 }
